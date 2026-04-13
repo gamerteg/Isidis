5898f4afeaf20854c40a13f12c0a5d4f3ba77969
Innerworks <cristian071932@gmail.com>
2026-04-13 18:17:35 -0300

    Docker file da api

--- isidis-monorepo/api/src/handler.ts
+++ isidis-monorepo/api/src/handler.ts
@@ -3,29 +3,16 @@ import type { FastifyInstance } from 'fastify'
 import { build } from './app.js'
 
 let app: FastifyInstance | null = null
-let initError: unknown = null
 
 async function getApp(): Promise<FastifyInstance> {
-  if (initError) throw initError
   if (!app) {
-    try {
-      app = await build()
-      await app.ready()
-    } catch (err) {
-      initError = err
-      throw err
-    }
+    app = await build()
+    await app.ready()
   }
   return app
 }
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
-  try {
-    const fastify = await getApp()
-    fastify.server.emit('request', req, res)
-  } catch (err) {
-    res.statusCode = 500
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ error: 'Internal Server Error' }))
-  }
+  const fastify = await getApp()
+  fastify.server.emit('request', req, res)
 }
